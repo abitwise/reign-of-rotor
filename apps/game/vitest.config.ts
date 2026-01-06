1cb6e126189adacc27c034f571784460b2bf6b3f
Olger <abitwise@gmail.com>
2026-01-06 22:48:50 +0200

    Disable debugging in vitest.

--- apps/game/vitest.config.ts
+++ apps/game/vitest.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    globals: true
+    globals: true,
+    inspect: false
+  },
+  define: {
+    'import.meta.env.VITE_ENABLE_DEBUG': '"false"'
   }
 });
